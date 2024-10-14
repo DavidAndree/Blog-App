87f811430f0169bf5d242f0c2ca17725ecab534f
David Alvarado <davidalvaradoc0106@gmail.com>
2024-10-14 13:58:50 -0400

    Add Create, Update, and Delete. Add tests to verify functionality.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 
@@ -23,3 +25,20 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+    def get_initial(self):
+        """Get initial form data"""
+        # Get the result of calling the parent method
+        initial_data = super().get_initial()
+        # Add the author so it is the requests user
+        initial_data["author"] = self.request.user
+        # Return the data.
+        return initial_data
+
+
+class BlogDeleteView(DeleteView):
+    """Blog Delete View"""
+
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
